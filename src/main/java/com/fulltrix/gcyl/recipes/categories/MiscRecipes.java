4e8a62597c600b4fbddeeba29d73c0d36ad6e53d
brian <brianbosseyt@gmail.com>
2024-06-10 07:33:17 -0500

    Dimension based rock breaker recipes for netherrack and endstone

--- src/main/java/com/fulltrix/gcyl/recipes/categories/MiscRecipes.java
+++ src/main/java/com/fulltrix/gcyl/recipes/categories/MiscRecipes.java
@@ -1,13 +1,15 @@
 package com.fulltrix.gcyl.recipes.categories;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+
 import static com.fulltrix.gcyl.item.GCYLCoreItems.COVER_ENDER_ITEM_LINK;
 import static com.fulltrix.gcyl.materials.GCYLMaterials.*;
 import static com.fulltrix.gcyl.materials.GCYLNuclearMaterials.Americium241;
 import static com.fulltrix.gcyl.recipes.GCYLRecipeMaps.ADVANCED_MIXER_RECIPES;
 import static gregicality.multiblocks.api.unification.GCYMMaterials.Stellite100;
 import static gregtech.api.GTValues.*;
-import static gregtech.api.recipes.RecipeMaps.ASSEMBLER_RECIPES;
-import static gregtech.api.recipes.RecipeMaps.MIXER_RECIPES;
+import static gregtech.api.recipes.RecipeMaps.*;
 import static gregtech.api.unification.material.Materials.*;
 import static gregtech.api.unification.material.Materials.Bismuth;
 import static gregtech.api.unification.ore.OrePrefix.*;
@@ -49,6 +51,23 @@ public class MiscRecipes {
                 .fluidInputs(Polyethylene.getFluid(L * 2))
                 .output(COVER_ENDER_ITEM_LINK)
                 .EUt(VA[HV]).duration(320)
+                .buildAndRegister();
+
+        //Rock breaker recipes for netherrack and endstone
+        ROCK_BREAKER_RECIPES.recipeBuilder()
+                .notConsumable(new ItemStack(Blocks.NETHERRACK))
+                .duration(16)
+                .EUt(240)
+                .dimension(-1)
+                .outputs(new ItemStack(Blocks.NETHERRACK))
+                .buildAndRegister();
+
+        ROCK_BREAKER_RECIPES.recipeBuilder()
+                .notConsumable(new ItemStack(Blocks.END_STONE))
+                .duration(16)
+                .EUt(960)
+                .dimension(1)
+                .outputs(new ItemStack(Blocks.END_STONE))
                 .buildAndRegister();
     }
 
