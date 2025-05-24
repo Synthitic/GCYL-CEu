03c09d67e78e0627f493fbf387f43d4472ee1ac2
brian <brianbosseyt@gmail.com>
2025-05-24 20:46:18 +0900

    ybco fix

--- src/main/java/com/fulltrix/gcyl/recipes/categories/RecipeOverrideLate.java
+++ src/main/java/com/fulltrix/gcyl/recipes/categories/RecipeOverrideLate.java
@@ -5,13 +5,15 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 
 import static com.fulltrix.gcyl.api.GCYLUtility.removeExtractorRecipe;
+import static com.fulltrix.gcyl.materials.GCYLMaterials.QCDMatter;
 import static com.fulltrix.gcyl.materials.chains.NewPlatinumGroupMaterials.PotassiumBisulfate;
 import static gregtech.api.recipes.GTRecipeHandler.removeRecipesByInputs;
-import static gregtech.api.recipes.RecipeMaps.ARC_FURNACE_RECIPES;
-import static gregtech.api.recipes.RecipeMaps.AUTOCLAVE_RECIPES;
+import static gregtech.api.recipes.RecipeMaps.*;
+import static gregtech.api.recipes.ingredients.IntCircuitIngredient.getIntegratedCircuit;
 import static gregtech.api.unification.material.Materials.*;
 import static gregtech.api.unification.material.Materials.Oxygen;
 import static gregtech.api.unification.ore.OrePrefix.dust;
+import static gregtech.common.items.MetaItems.*;
 
 public class RecipeOverrideLate {
     public static void init() {
@@ -25,5 +27,11 @@ public class RecipeOverrideLate {
 
         //monazite autoclave
         removeRecipesByInputs(AUTOCLAVE_RECIPES, new ItemStack[]{OreDictUnifier.get(dust, Monazite)}, new FluidStack[]{Water.getFluid(250)});
+
+        //yttrium barium cuprate dust again for autogen
+        removeRecipesByInputs(MIXER_RECIPES, new ItemStack[]{getIntegratedCircuit(4), OreDictUnifier.get(dust, Yttrium), OreDictUnifier.get(dust, Barium, 2), OreDictUnifier.get(dust, Copper,3)}, new FluidStack[]{Oxygen.getFluid(7000)});
+
+
+
     }
 }
