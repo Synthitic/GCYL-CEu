bf702ac2673ea26affb5f6c9b8557c6976d0ac14
Johny709 <86203059+Johny709@users.noreply.github.com>
2025-12-22 06:26:39 -0500

    Fix and finish Large Rocket Engine UI and tooltip. Also buff energy output

--- src/main/java/com/fulltrix/gcyl/machines/multi/generator/MetaTileEntityLargeRocketEngine.java
+++ src/main/java/com/fulltrix/gcyl/machines/multi/generator/MetaTileEntityLargeRocketEngine.java
@@ -1,6 +1,8 @@
 package com.fulltrix.gcyl.machines.multi.generator;
 
+import com.cleanroommc.modularui.api.drawable.IKey;
 import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.cleanroommc.modularui.value.sync.StringSyncValue;
 import com.fulltrix.gcyl.blocks.metal.MetalCasing1;
 import com.fulltrix.gcyl.api.recipes.GCYLRecipeMaps;
 import gregtech.api.GTValues;
@@ -10,33 +12,58 @@ import gregtech.api.fluids.store.FluidStorageKeys;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.*;
+import gregtech.api.metatileentity.multiblock.ui.MultiblockUIBuilder;
 import gregtech.api.metatileentity.multiblock.ui.TemplateBarBuilder;
+import gregtech.api.mui.GTGuiTextures;
+import gregtech.api.mui.sync.FixedIntArraySyncValue;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
 import gregtech.api.unification.material.Materials;
+import gregtech.api.util.GTUtility;
 import gregtech.client.renderer.ICubeRenderer;
 import gregtech.common.blocks.BlockMultiblockCasing;
 import gregtech.common.blocks.MetaBlocks;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.function.UnaryOperator;
 
 import static com.fulltrix.gcyl.client.ClientHandler.NITINOL_60_CASING;
 import static com.fulltrix.gcyl.blocks.GCYLMetaBlocks.METAL_CASING_1;
-import static gregtech.api.GTValues.LuV;
+import static gregtech.api.GTValues.UV;
 import static gregtech.api.util.RelativeDirection.*;
 
-//TODO: implement ui, tooltip
 
 public class MetaTileEntityLargeRocketEngine extends FuelMultiblockController implements ProgressBarMultiblock {
 
     public MetaTileEntityLargeRocketEngine(ResourceLocation metaTileEntityId) {
-        super(metaTileEntityId, GCYLRecipeMaps.ROCKET_FUEL_RECIPES, LuV);
-        this.recipeMapWorkable.setMaximumOverclockVoltage(GTValues.V[LuV]);
+        super(metaTileEntityId, GCYLRecipeMaps.ROCKET_FUEL_RECIPES, UV);
+        this.recipeMapWorkable.setMaximumOverclockVoltage(655360);
         this.recipeMapWorkable = new MetaTileEntityLargeRocketEngine.LREWorkableHandler(this);
+    }
+
+    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
+        return new MetaTileEntityLargeRocketEngine(metaTileEntityId);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, @Nullable World world, @NotNull List<String> tooltip, boolean advanced) {
+        super.addInformation(stack, world, tooltip, advanced);
+        tooltip.add(I18n.format("gcyl.multiblock.large_rocket_engine.tooltip.1"));
+        tooltip.add(I18n.format("gcyl.multiblock.large_rocket_engine.tooltip.2"));
+        tooltip.add(I18n.format("gcyl.multiblock.large_rocket_engine.tooltip.3"));
     }
 
     @Override
@@ -60,24 +87,96 @@ public class MetaTileEntityLargeRocketEngine extends FuelMultiblockController im
     }
 
     @Override
+    @SideOnly(Side.CLIENT)
     public ICubeRenderer getBaseTexture(IMultiblockPart iMultiblockPart) {
         return NITINOL_60_CASING;
     }
 
-    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
-        return new MetaTileEntityLargeRocketEngine(metaTileEntityId);
+    @Override
+    public @NotNull List<ITextComponent> getDataInfo() {
+        List<ITextComponent> list = super.getDataInfo();
+        if (((LREWorkableHandler) this.recipeMapWorkable).isOxygenBoosted())
+            list.add(new TextComponentTranslation("gregtech.multiblock.universal.generator.boosted"));
+        return list;
+    }
+
+    @Override
+    protected void configureDisplayText(MultiblockUIBuilder builder) {
+        super.configureDisplayText(builder);
+        LREWorkableHandler recipeLogic = (LREWorkableHandler)this.recipeMapWorkable;
+        builder.addProgressLine(recipeLogic.getProgress(), recipeLogic.getMaxProgress());
+        builder.addCustom((key, syncer) -> {
+           long EUt = syncer.syncLong(recipeLogic.getInfoProviderEUt());
+           int tier = GTUtility.getFloorTierByVoltage(EUt) + 1;
+           if (EUt > 0)
+               key.add(IKey.lang("gregtech.multiblock.universal.energy.production", EUt, GTValues.VOCNF[tier]));
+           if (syncer.syncBoolean(recipeLogic.isOxygenBoosted()))
+               key.add(IKey.lang("gregtech.multiblock.universal.generator.boosted"));
+        });
     }
 
     @Override
     public int getProgressBarCount() {
-        return 0;
+        return 3;
     }
 
     @Override
-    public void registerBars(List<UnaryOperator<TemplateBarBuilder>> list, PanelSyncManager panelSyncManager) {
+    public void registerBars(List<UnaryOperator<TemplateBarBuilder>> bars, PanelSyncManager syncManager) {
+        StringSyncValue fuelNameValue = new StringSyncValue(() -> {
+            FluidStack stack = ((MultiblockFuelRecipeLogic) recipeMapWorkable).getInputFluidStack();
+            return stack == null || stack.getFluid() == null ? null : stack.getFluid().getName();
+        });
+        syncManager.syncValue("fuel_name", fuelNameValue);
+        FixedIntArraySyncValue fuelValue = new FixedIntArraySyncValue(this::getFuelAmount, null);
+        syncManager.syncValue("fuel_amount", fuelValue);
+        FixedIntArraySyncValue airValue = new FixedIntArraySyncValue(this::getAirAmount, null);
+        syncManager.syncValue("air_amount", airValue);
+        FixedIntArraySyncValue liquidOxygenValue = new FixedIntArraySyncValue(this::getLiquidOxygenAmount, null);
+        syncManager.syncValue("liquid_oxygen_amount", liquidOxygenValue);
 
+        bars.add(bar -> bar.progress(() -> fuelValue.getValue(1) == 0 ? 0 : 1.0 * fuelValue.getValue(0) / fuelValue.getValue(1))
+                .texture(GTGuiTextures.PROGRESS_BAR_LCE_FUEL)
+                .tooltipBuilder(tooltip -> this.createFuelTooltip(tooltip, fuelValue, fuelNameValue)));
+        bars.add(bar -> bar.progress(() -> airValue.getValue(1) == 0 ? 0 : 1.0 * airValue.getValue(0) / airValue.getValue(1))
+                .texture(GTGuiTextures.PROGRESS_BAR_LCE_LUBRICANT)
+                .tooltipBuilder(tooltip -> tooltip.addLine(!this.isStructureFormed() ? IKey.lang("gregtech.multiblock.invalid_structure")
+                        : airValue.getValue(0) == 0 ? IKey.lang("gcyl.multiblock.large_rocket_engine.air_none")
+                        : IKey.lang("gcyl.multiblock.large_rocket_engine.air_amount", airValue.getValue(0), airValue.getValue(1)))));
+        bars.add(bar -> bar.progress(() -> liquidOxygenValue.getValue(1) == 0 ? 0 : 1.0 * liquidOxygenValue.getValue(0) / liquidOxygenValue.getValue(1))
+                .texture(GTGuiTextures.PROGRESS_BAR_LCE_OXYGEN)
+                .tooltipBuilder(tooltip -> tooltip.addLine(!this.isStructureFormed() ? IKey.lang("gregtech.multiblock.invalid_structure")
+                        : liquidOxygenValue.getValue(0) == 0 ? IKey.lang("gregtech.multiblock.large_combustion_engine.oxygen_none")
+                        : IKey.lang("gregtech.multiblock.large_combustion_engine.liquid_oxygen_amount", liquidOxygenValue.getValue(0), liquidOxygenValue.getValue(1)))));
     }
 
+    /**
+     * @return an array of [fuel stored, fuel capacity]
+     */
+    private int[] getFuelAmount() {
+        if (this.getInputFluidInventory() != null) {
+            MultiblockFuelRecipeLogic recipeLogic = (MultiblockFuelRecipeLogic) recipeMapWorkable;
+            if (recipeLogic.getInputFluidStack() != null) {
+                FluidStack testStack = recipeLogic.getInputFluidStack().copy();
+                testStack.amount = Integer.MAX_VALUE;
+                return this.getTotalFluidAmount(testStack, this.getInputFluidInventory());
+            }
+        }
+        return new int[2];
+    }
+
+    /**
+     * @return an array of [fuel stored, fuel capacity]
+     */
+    private int[] getAirAmount() {
+        return this.getInputFluidInventory() != null ? this.getTotalFluidAmount(Materials.Air.getFluid(Integer.MAX_VALUE), this.getInputFluidInventory()) : new int[2];
+    }
+
+    /**
+     * @return an array of [fuel stored, fuel capacity]
+     */
+    private int[] getLiquidOxygenAmount() {
+        return this.getInputFluidInventory() != null ? this.getTotalFluidAmount(Materials.Oxygen.getFluid(FluidStorageKeys.LIQUID, Integer.MAX_VALUE), this.getInputFluidInventory()) : new int[2];
+    }
 
     public static class LREWorkableHandler extends MultiblockFuelRecipeLogic {
 
@@ -85,10 +184,8 @@ public class MetaTileEntityLargeRocketEngine extends FuelMultiblockController im
 
         private final MetaTileEntityLargeRocketEngine rocketEngine;
 
-        private final int tier = LuV;
-
-        private static final FluidStack OXYGEN_STACK = Materials.Oxygen.getFluid(FluidStorageKeys.LIQUID,50);
-        private static final FluidStack AIR_STACK = Materials.Air.getFluid(37500);
+        public static final FluidStack OXYGEN_STACK = Materials.Oxygen.getFluid(FluidStorageKeys.LIQUID,50);
+        public static final FluidStack AIR_STACK = Materials.Air.getFluid(37500);
 
         public LREWorkableHandler(RecipeMapMultiblockController tileEntity) {
             super(tileEntity);
@@ -150,18 +247,12 @@ public class MetaTileEntityLargeRocketEngine extends FuelMultiblockController im
         @Override
         public long getMaxVoltage() {
             // this multiplies consumption through parallel
-            if (isOxygenBoosted)
-                return GTValues.V[tier] * 3;
-            else
-                return GTValues.V[tier];
+            return this.isOxygenBoosted ? 655360 * 3 : 655360;
         }
 
         @Override
         protected long boostProduction(long production) {
             // this multiplies production without increasing consumption
-            if (isOxygenBoosted)
-                return production;
-
             return production;
         }
 
@@ -169,6 +260,10 @@ public class MetaTileEntityLargeRocketEngine extends FuelMultiblockController im
         public void invalidate() {
             isOxygenBoosted = false;
             super.invalidate();
+        }
+
+        public boolean isOxygenBoosted() {
+            return this.isOxygenBoosted;
         }
     }
 }
