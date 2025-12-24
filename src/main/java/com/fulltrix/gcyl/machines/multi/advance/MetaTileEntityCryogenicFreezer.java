86e7229311af48bcf0429b07a7bc0a5e2ee1c672
Johny709 <86203059+Johny709@users.noreply.github.com>
2025-12-24 16:50:22 -0500

    Cryogenic Freezer progress

--- src/main/java/com/fulltrix/gcyl/machines/multi/advance/MetaTileEntityCryogenicFreezer.java
+++ src/main/java/com/fulltrix/gcyl/machines/multi/advance/MetaTileEntityCryogenicFreezer.java
@@ -1,20 +1,27 @@
 package com.fulltrix.gcyl.machines.multi.advance;
 
+import com.cleanroommc.modularui.api.drawable.IKey;
+import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.fulltrix.gcyl.api.multi.GCYLRecipeMapMultiblockController;
 import com.fulltrix.gcyl.materials.GCYLMaterials;
 import com.fulltrix.gcyl.client.ClientHandler;
 import gregicality.multiblocks.api.capability.impl.GCYMMultiblockRecipeLogic;
-import gregicality.multiblocks.api.metatileentity.GCYMRecipeMapMultiblockController;
 import gregicality.multiblocks.api.render.GCYMTextures;
 import gregicality.multiblocks.common.block.GCYMMetaBlocks;
 import gregicality.multiblocks.common.block.blocks.BlockLargeMultiblockCasing;
 import gregtech.api.GTValues;
-import gregtech.api.capability.IMultipleTankHandler;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.ProgressBarMultiblock;
 import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
+import gregtech.api.metatileentity.multiblock.ui.TemplateBarBuilder;
+import gregtech.api.mui.GTGuiTextures;
+import gregtech.api.mui.sync.FixedIntArraySyncValue;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.PatternMatchContext;
+import gregtech.api.recipes.RecipeBuilder;
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.api.util.GTUtility;
 import gregtech.client.renderer.ICubeRenderer;
@@ -27,30 +34,39 @@ import net.minecraft.world.World;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import java.util.List;
+import java.util.function.UnaryOperator;
 
 import static gregtech.api.GTValues.ZPM;
 
 //TODO: limit reflect in ui. update tooltip. improve performance
 
-public class MetaTileEntityCryogenicFreezer extends GCYMRecipeMapMultiblockController {
+public class MetaTileEntityCryogenicFreezer extends GCYLRecipeMapMultiblockController implements ProgressBarMultiblock {
 
-    protected final MetaTileEntity metaTileEntity;
+    private static final FluidStack CRYOTHEUM = GCYLMaterials.Cryotheum.getFluid(Integer.MAX_VALUE);
+
+    private FluidStack cryotheum;
 
     public MetaTileEntityCryogenicFreezer(ResourceLocation metaTileEntityId) {
-        super(metaTileEntityId, RecipeMaps.VACUUM_RECIPES);
+        super(metaTileEntityId, RecipeMaps.VACUUM_RECIPES, false);
         this.recipeMapWorkable = new MetaTileEntityCryogenicFreezer.CryogenicRecipeLogic(this);
-        this.metaTileEntity = this;
         this.recipeMapWorkable.setMaximumOverclockVoltage(Math.min(this.energyContainer.getInputVoltage() , GTValues.V[ZPM]));
     }
 
     @Override
     public MetaTileEntity createMetaTileEntity(IGregTechTileEntity metaTileEntityHolder) {
         return new MetaTileEntityCryogenicFreezer(this.metaTileEntityId);
+    }
+
+    @Override
+    protected void formStructure(PatternMatchContext context) {
+        super.formStructure(context);
+        this.cryotheum = GCYLMaterials.Cryotheum.getFluid((int) Math.pow(2, GTUtility.getTierByVoltage(this.energyContainer.getInputVoltage())));
     }
 
     @Override
@@ -86,8 +102,6 @@ public class MetaTileEntityCryogenicFreezer extends GCYMRecipeMapMultiblockContr
         return ClientHandler.FREEZER_OVERLAY;
     }
 
-
-
     @Override
     @SideOnly(Side.CLIENT)
     public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, boolean advanced) {
@@ -96,52 +110,57 @@ public class MetaTileEntityCryogenicFreezer extends GCYMRecipeMapMultiblockContr
         tooltip.add(I18n.format("gregtech.multiblock.vol_cryo.description"));
     }
 
+    @Override
+    public int getProgressBarCount() {
+        return 1;
+    }
+
+    @Override
+    public void registerBars(List<UnaryOperator<TemplateBarBuilder>> bars, PanelSyncManager syncManager) {
+        FixedIntArraySyncValue cryotheumAmount = new FixedIntArraySyncValue(this::getCryotheumAmount, null);
+        syncManager.syncValue("cryotheum_amount", cryotheumAmount);
+
+        bars.add(bar -> bar.progress(() -> cryotheumAmount.getValue(1) == 0 ? 0 : 1.0 * cryotheumAmount.getValue(0) / cryotheumAmount.getValue(1))
+                .texture(GTGuiTextures.PROGRESS_BAR_LCE_OXYGEN)
+                .tooltipBuilder(tooltip -> tooltip.addLine(!this.isStructureFormed() ? IKey.lang("gregtech.multiblock.invalid_structure")
+                        : cryotheumAmount.getValue(0) == 0 ? IKey.lang("gregtech.multiblock.large_combustion_engine.fuel_none")
+                        : IKey.lang("gcyl.multiblock.void_miner.cryotheum", cryotheumAmount.getValue(0), cryotheumAmount.getValue(1)))));
+    }
+
+    private int[] getCryotheumAmount() {
+        return this.getInputFluidInventory() != null ? this.getTotalFluidAmount(CRYOTHEUM, this.getInputFluidInventory()) : new int[2];
+    }
 
     private class CryogenicRecipeLogic extends GCYMMultiblockRecipeLogic {
 
-        private final MetaTileEntityCryogenicFreezer freezer;
-
         public CryogenicRecipeLogic(RecipeMapMultiblockController metaTileEntity) {
             super(metaTileEntity);
-            this.freezer = (MetaTileEntityCryogenicFreezer) metaTileEntity;
-        }
-
-        protected FluidStack getCryotheum() {
-            return GCYLMaterials.Cryotheum.getFluid((int) Math.pow(2, GTUtility.getTierByVoltage(this.freezer.energyContainer.getInputVoltage())));
-        }
-
-        protected boolean checkCryotheum() {
-            IMultipleTankHandler inputTank = freezer.getInputFluidInventory();
-            if(getCryotheum().isFluidStackIdentical(inputTank.drain(getCryotheum(), false))) {
-                return true;
-            }
-            else {
-                invalidate();
-                return false;
-            }
-        }
-
-        protected void drainCryotheum() {
-            freezer.getInputFluidInventory().drain(getCryotheum(), true);
         }
 
         @Override
-        protected boolean shouldSearchForRecipes() {
-            return super.shouldSearchForRecipes() && checkCryotheum();
+        protected void updateRecipeProgress() {
+            if (cryotheum.isFluidStackIdentical(this.getInputTank().drain(cryotheum, false))) {
+                this.getInputTank().drain(cryotheum, true);
+                super.updateRecipeProgress();
+            } else this.decreaseProgress();
         }
 
         @Override
-        protected boolean canProgressRecipe() {
-            if(checkCryotheum() && freezer.isActive()) {
-                drainCryotheum();
-            }
-
-            return super.canProgressRecipe() && checkCryotheum();
+        public long getMaxParallelVoltage() {
+            return Long.MAX_VALUE;
         }
 
         @Override
-        public long getMaximumOverclockVoltage() {
-            return Math.min(freezer.getEnergyContainer().getInputVoltage(), GTValues.V[ZPM]);
+        public int getParallelLimit() {
+            return 8;
+        }
+
+        @Override
+        public void applyParallelBonus(@NotNull RecipeBuilder<?> builder) {
+            long EUt = (long) (builder.getEUt() * 0.9);
+
+            builder.EUt(EUt / builder.getParallel())
+                    .duration((int) (builder.getDuration() * (1.0F / 2.2F)));
         }
     }
 }
