29ab9e9175b713cedacc6ba4b4309bfb06bc90fa
Johny709 <janjoseph.merida@gmail.com>
2025-12-24 22:46:14 -0500

    Fix Decay Chamber voltage casings

--- src/main/java/com/fulltrix/gcyl/machines/multi/simple/MetaTileEntityDecayChamber.java
+++ src/main/java/com/fulltrix/gcyl/machines/multi/simple/MetaTileEntityDecayChamber.java
@@ -1,14 +1,19 @@
 package com.fulltrix.gcyl.machines.multi.simple;
 
+import com.fulltrix.gcyl.api.multi.GCYLMultiblockRecipeLogic;
 import com.fulltrix.gcyl.api.multi.GCYLRecipeMapMultiblockController;
+import com.fulltrix.gcyl.api.pattern.TraceabilityPredicates;
 import com.fulltrix.gcyl.client.ClientHandler;
 import com.fulltrix.gcyl.blocks.GCYLMetaBlocks;
 import com.fulltrix.gcyl.blocks.GCYLReactorCasing;
+import gregicality.multiblocks.common.GCYMConfigHolder;
+import gregicality.multiblocks.common.metatileentities.multiblockpart.MetaTileEntityTieredHatch;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.PatternMatchContext;
 import gregtech.client.renderer.ICubeRenderer;
 import gregtech.client.renderer.texture.Textures;
 import gregtech.common.blocks.BlockGlassCasing;
@@ -19,6 +24,8 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+
 import static com.fulltrix.gcyl.api.recipes.GCYLRecipeMaps.DECAY_CHAMBERS_RECIPES;
 
 
@@ -26,8 +33,16 @@ import static com.fulltrix.gcyl.api.recipes.GCYLRecipeMaps.DECAY_CHAMBERS_RECIPE
 //TODO: add tooltips and information
 public class MetaTileEntityDecayChamber extends GCYLRecipeMapMultiblockController {
 
+    private long maxVoltage;
+
     public MetaTileEntityDecayChamber(ResourceLocation metaTileEntityId, boolean isParallel) {
         super(metaTileEntityId, DECAY_CHAMBERS_RECIPES, isParallel);
+        this.recipeMapWorkable = new GCYLMultiblockRecipeLogic(this, false) {
+            @Override
+            public long getMaxVoltage() {
+                return GCYMConfigHolder.globalMultiblocks.enableTieredCasings ? maxVoltage : super.getMaxVoltage();
+            }
+        };
     }
 
     @Override
@@ -40,8 +55,21 @@ public class MetaTileEntityDecayChamber extends GCYLRecipeMapMultiblockControlle
                 .where('S', selfPredicate())
                 .where('G', states(getGlassState()))
                 .where('#', air())
-                .where('T', tieredCasing())
+                .where('T', TraceabilityPredicates.tieredHatchPredicate())
                 .build();
+    }
+
+    @Override
+    protected void formStructure(PatternMatchContext context) {
+        super.formStructure(context);
+        int tier = context.getOrDefault("tiered_hatches", new ArrayList<MetaTileEntityTieredHatch>()).get(0).getTier() - 1;
+        this.maxVoltage = 32L << tier * 2;
+    }
+
+    @Override
+    public void invalidateStructure() {
+        super.invalidateStructure();
+        this.maxVoltage = 0;
     }
 
     public IBlockState getCasingState() {
