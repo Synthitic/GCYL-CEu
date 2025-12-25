81479ba250c98fc3b4b2d051be090e77f1c166a8
Johny709 <86203059+Johny709@users.noreply.github.com>
2025-12-25 15:56:24 -0500

    Fix Adv Centrifuge voltage casings

--- src/main/java/com/fulltrix/gcyl/machines/multi/advance/MetaTileEntityAdvCentrifuge.java
+++ src/main/java/com/fulltrix/gcyl/machines/multi/advance/MetaTileEntityAdvCentrifuge.java
@@ -1,34 +1,54 @@
 package com.fulltrix.gcyl.machines.multi.advance;
 
+import com.fulltrix.gcyl.api.multi.GCYLMultiblockRecipeLogic;
+import com.fulltrix.gcyl.api.multi.GCYLRecipeMapMultiblockController;
+import com.fulltrix.gcyl.api.pattern.TraceabilityPredicates;
 import com.fulltrix.gcyl.api.recipes.GCYLRecipeMaps;
-import gregicality.multiblocks.api.metatileentity.GCYMRecipeMapMultiblockController;
 import gregicality.multiblocks.api.render.GCYMTextures;
+import gregicality.multiblocks.common.GCYMConfigHolder;
 import gregicality.multiblocks.common.block.GCYMMetaBlocks;
 import gregicality.multiblocks.common.block.blocks.BlockLargeMultiblockCasing;
-import gregicality.multiblocks.common.metatileentities.multiblock.standard.MetaTileEntityLargeCentrifuge;
+import gregicality.multiblocks.common.metatileentities.GCYMMetaTileEntities;
+import gregicality.multiblocks.common.metatileentities.multiblockpart.MetaTileEntityTieredHatch;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.MultiblockShapeInfo;
+import gregtech.api.pattern.PatternMatchContext;
 import gregtech.api.recipes.RecipeMap;
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.client.renderer.ICubeRenderer;
 import gregtech.client.renderer.texture.cube.OrientedOverlayRenderer;
 import gregtech.common.blocks.BlockBoilerCasing;
 import gregtech.common.blocks.MetaBlocks;
+import gregtech.common.metatileentities.MetaTileEntities;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static gregtech.api.util.RelativeDirection.*;
 
 //TODO: remove centrifuge map when small recipe maps work
-public class MetaTileEntityAdvCentrifuge extends GCYMRecipeMapMultiblockController {
+public class MetaTileEntityAdvCentrifuge extends GCYLRecipeMapMultiblockController {
+
+    private long maxVoltage;
 
     public MetaTileEntityAdvCentrifuge(ResourceLocation metaTileEntityId) {
-        super(metaTileEntityId,
-                new RecipeMap[] { RecipeMaps.THERMAL_CENTRIFUGE_RECIPES, GCYLRecipeMaps.ADVANCED_CENTRIFUGE_RECIPES, RecipeMaps.CENTRIFUGE_RECIPES });
+        super(metaTileEntityId, new RecipeMap[] { RecipeMaps.THERMAL_CENTRIFUGE_RECIPES, GCYLRecipeMaps.ADVANCED_CENTRIFUGE_RECIPES, RecipeMaps.CENTRIFUGE_RECIPES }, true);
+        this.recipeMapWorkable = new GCYLMultiblockRecipeLogic(this, false) {
+            @Override
+            public long getMaxVoltage() {
+                return GCYMConfigHolder.globalMultiblocks.enableTieredCasings ? maxVoltage : super.getMaxVoltage();
+            }
+        };
     }
 
     @Override
@@ -45,10 +65,44 @@ public class MetaTileEntityAdvCentrifuge extends GCYMRecipeMapMultiblockControll
                 .where('S', selfPredicate())
                 .where('X', states(getCasingState()).setMinGlobalLimited(40).or(autoAbilities()))
                 .where('C', states(getCasingState2()))
-                .where('T', tieredCasing().or(air()))
+                .where('T', TraceabilityPredicates.tieredHatchPredicate())
                 .where('A', air())
                 .where('#', any())
                 .build();
+    }
+
+    @Override
+    public List<MultiblockShapeInfo> getMatchingShapes() {
+        MultiblockShapeInfo.Builder builder = MultiblockShapeInfo.builder(RIGHT, BACK, DOWN)
+                .aisle("#XXX#", "XXXXX", "XXXXX", "XXXXX", "#XXX#")
+                .aisle("iISOo", "XACAX", "XCTCX", "XACAX", "XPEMX")
+                .aisle("#XXX#", "XXXXX", "XXXXX", "XXXXX", "#XXX#")
+                .where('S', this, EnumFacing.SOUTH)
+                .where('I', MetaTileEntities.ITEM_IMPORT_BUS[3], EnumFacing.SOUTH)
+                .where('O', MetaTileEntities.ITEM_EXPORT_BUS[3], EnumFacing.SOUTH)
+                .where('i', MetaTileEntities.FLUID_IMPORT_HATCH[3], EnumFacing.SOUTH)
+                .where('o', MetaTileEntities.ITEM_EXPORT_BUS[3], EnumFacing.SOUTH)
+                .where('P', GCYMMetaTileEntities.PARALLEL_HATCH[0], EnumFacing.NORTH)
+                .where('M', MetaTileEntities.MAINTENANCE_HATCH, EnumFacing.NORTH)
+                .where('X', this.getCasingState())
+                .where('C', this.getCasingState2());
+        return Arrays.stream(GCYMMetaTileEntities.TIERED_HATCH)
+                .map(hatch -> builder.where('T', hatch, EnumFacing.DOWN)
+                        .where('E', MetaTileEntities.ENERGY_INPUT_HATCH[hatch.getTier()], EnumFacing.NORTH).build())
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    protected void formStructure(PatternMatchContext context) {
+        super.formStructure(context);
+        int tier = context.getOrDefault("tiered_hatches", new ArrayList<MetaTileEntityTieredHatch>()).get(0).getTier() - 1;
+        this.maxVoltage = 32L << tier * 2;
+    }
+
+    @Override
+    public void invalidateStructure() {
+        super.invalidateStructure();
+        this.maxVoltage = 0;
     }
 
     private IBlockState getCasingState() {
