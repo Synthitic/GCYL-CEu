4480bb8e684b10daadf0819465e2089c4555b0d1
Johny709 <86203059+Johny709@users.noreply.github.com>
2025-12-25 16:35:35 -0500

    Fix Adv Mixer voltage casings

--- src/main/java/com/fulltrix/gcyl/machines/multi/advance/MetaTileEntityAdvMixer.java
+++ src/main/java/com/fulltrix/gcyl/machines/multi/advance/MetaTileEntityAdvMixer.java
@@ -1,17 +1,23 @@
 package com.fulltrix.gcyl.machines.multi.advance;
 
+import com.fulltrix.gcyl.api.multi.GCYLMultiblockRecipeLogic;
+import com.fulltrix.gcyl.api.multi.GCYLRecipeMapMultiblockController;
+import com.fulltrix.gcyl.api.pattern.TraceabilityPredicates;
 import com.fulltrix.gcyl.api.recipes.GCYLRecipeMaps;
-import gregicality.multiblocks.api.metatileentity.GCYMRecipeMapMultiblockController;
 import gregicality.multiblocks.api.render.GCYMTextures;
 import gregicality.multiblocks.api.unification.GCYMMaterials;
+import gregicality.multiblocks.common.GCYMConfigHolder;
 import gregicality.multiblocks.common.block.GCYMMetaBlocks;
 import gregicality.multiblocks.common.block.blocks.BlockLargeMultiblockCasing;
-import gregicality.multiblocks.common.metatileentities.multiblock.standard.MetaTileEntityLargeMixer;
+import gregicality.multiblocks.common.metatileentities.GCYMMetaTileEntities;
+import gregicality.multiblocks.common.metatileentities.multiblockpart.MetaTileEntityTieredHatch;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.MultiblockShapeInfo;
+import gregtech.api.pattern.PatternMatchContext;
 import gregtech.api.recipes.RecipeMap;
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.client.renderer.ICubeRenderer;
@@ -20,18 +26,34 @@ import gregtech.common.blocks.BlockBoilerCasing;
 import gregtech.common.blocks.BlockMetalCasing;
 import gregtech.common.blocks.BlockTurbineCasing;
 import gregtech.common.blocks.MetaBlocks;
+import gregtech.common.metatileentities.MetaTileEntities;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static gregtech.api.util.RelativeDirection.*;
+
 
 //TODO: remove regular mixer recipes once small map is implemented
-public class MetaTileEntityAdvMixer extends GCYMRecipeMapMultiblockController {
+public class MetaTileEntityAdvMixer extends GCYLRecipeMapMultiblockController {
+
+    private long maxVoltage;
 
     public MetaTileEntityAdvMixer(ResourceLocation metaTileEnttityID) {
-        super(metaTileEnttityID,  new RecipeMap[] {RecipeMaps.MIXER_RECIPES, GCYLRecipeMaps.ADVANCED_MIXER_RECIPES});
+        super(metaTileEnttityID,  new RecipeMap[] {RecipeMaps.MIXER_RECIPES, GCYLRecipeMaps.ADVANCED_MIXER_RECIPES}, true);
+        this.recipeMapWorkable = new GCYLMultiblockRecipeLogic(this, false) {
+            @Override
+            public long getMaxVoltage() {
+                return GCYMConfigHolder.globalMultiblocks.enableTieredCasings ? maxVoltage : super.getMaxVoltage();
+            }
+        };
     }
-
 
     @Override
     public MetaTileEntity createMetaTileEntity(IGregTechTileEntity metaTileEntityHolder) {
@@ -50,11 +72,49 @@ public class MetaTileEntityAdvMixer extends GCYMRecipeMapMultiblockController {
                 .where('X', states(getCasingState()).setMinGlobalLimited(50).or(autoAbilities()))
                 .where('P', states(getCasingState2()))
                 .where('C', states(getCasingState3()))
-                .where('T', tieredCasing().or(states(getCasingState4())))
+                .where('T', TraceabilityPredicates.tieredHatchPredicate())
                 .where('F', frames(GCYMMaterials.HastelloyX))
                 .where('A', air())
                 .where('#', any())
                 .build();
+    }
+
+    @Override
+    public List<MultiblockShapeInfo> getMatchingShapes() {
+        MultiblockShapeInfo.Builder builder = MultiblockShapeInfo.builder(RIGHT, DOWN, FRONT)
+                .aisle("#pEM#", "#XXX#", "#XXX#", "#XXX#", "#XXX#", "##F##")
+                .aisle("XXXXX", "XACAX", "XAAAX", "XACAX", "XAAAX", "##F##")
+                .aisle("XXXXX", "XCPCX", "XAPAX", "XCPCX", "XAPAX", "FFTFF")
+                .aisle("XXXXX", "iACAo", "XAAAX", "XACAX", "XAAAX", "##F##")
+                .aisle("#XXX#", "#ISO#", "#XXX#", "#XXX#", "#XXX#", "##F##")
+                .where('S', this, EnumFacing.SOUTH)
+                .where('I', MetaTileEntities.ITEM_IMPORT_BUS[3], EnumFacing.SOUTH)
+                .where('O', MetaTileEntities.ITEM_EXPORT_BUS[3], EnumFacing.SOUTH)
+                .where('i', MetaTileEntities.FLUID_IMPORT_HATCH[3], EnumFacing.SOUTH)
+                .where('o', MetaTileEntities.FLUID_EXPORT_HATCH[3], EnumFacing.SOUTH)
+                .where('p', GCYMMetaTileEntities.PARALLEL_HATCH[0], EnumFacing.NORTH)
+                .where('M', MetaTileEntities.MAINTENANCE_HATCH, EnumFacing.NORTH)
+                .where('X', this.getCasingState())
+                .where('P', this.getCasingState2())
+                .where('C', this.getCasingState3())
+                .where('F', MetaBlocks.FRAMES.get(GCYMMaterials.HastelloyX).getStateFromMeta(12));
+        return Arrays.stream(GCYMMetaTileEntities.TIERED_HATCH)
+                .map(hatch -> builder.where('T', hatch, EnumFacing.DOWN)
+                        .where('E', MetaTileEntities.ENERGY_INPUT_HATCH[hatch.getTier()], EnumFacing.NORTH).build())
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    protected void formStructure(PatternMatchContext context) {
+        super.formStructure(context);
+        int tier = context.getOrDefault("tiered_hatches", new ArrayList<MetaTileEntityTieredHatch>()).get(0).getTier() - 1;
+        this.maxVoltage = 32L << tier * 2;
+    }
+
+    @Override
+    public void invalidateStructure() {
+        super.invalidateStructure();
+        this.maxVoltage = 0;
     }
 
     private IBlockState getCasingState() {
@@ -81,11 +141,6 @@ public class MetaTileEntityAdvMixer extends GCYMRecipeMapMultiblockController {
     @Override
     protected @NotNull OrientedOverlayRenderer getFrontOverlay() {
         return GCYMTextures.LARGE_MIXER_OVERLAY;
-    }
-
-    @Override
-    public boolean canBeDistinct() {
-        return true;
     }
 
 }
